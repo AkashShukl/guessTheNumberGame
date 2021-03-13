351436d01fe5eb32403f2117104f24f99624aa01
shuklaa884 <shuklaa884@gmail.com>
2021-03-13 11:51:27 +0530

    Added new module core and configurations

---
+++ core/src/main/java/learn/application/Main.java
@@ -0,0 +1,13 @@
+package learn.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    public static  final Logger log = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        log.info("Inside the guess number game ");
+    }
+}
---
+++ core/src/main/java/learn/application/NumberGenerator.java
@@ -0,0 +1,8 @@
+package learn.application;
+
+public interface NumberGenerator {
+
+    int next();
+
+    int getNextNumber();
+}
