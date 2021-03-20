d706f725896eae626cbbbb7d0b188e333a987938
shuklaa884 <shuklaa884@gmail.com>
2021-03-20 13:05:16 +0530

    Implemented bean method in place of componnet scan: learned the difference and use cases between two.

--- core/src/main/java/learn/application/AppConfig.java
+++ core/src/main/java/learn/application/AppConfig.java
@@ -1,10 +1,21 @@
 package learn.application;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "learn.application")
 public class AppConfig {
+    //Bean method configuration implementation
+    @Bean
+    public NumberGenerator numberGenerator(){
+        return new NumberGeneratorImpl();
+    }
+
+    @Bean
+    public Game game(){
+        return new GameImpl();
+    }
 
 }
