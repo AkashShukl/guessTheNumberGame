f849377ecad339694a51d2dfea3150bb81e01cd7
shuklaa884 <shuklaa884@gmail.com>
2021-03-20 12:54:31 +0530

    Removed beans.xml file as from now on we will use Appconfig class for configuration management

---
+++ core/src/main/java/learn/application/AppConfig.java
@@ -0,0 +1,10 @@
+package learn.application;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "learn.application")
+public class AppConfig {
+
+}
--- core/src/main/java/learn/application/Main.java
+++ core/src/main/java/learn/application/Main.java
@@ -3,12 +3,13 @@ package learn.application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
-    private static final String CONFIG_LOCATION = "beans.xml";
+//    private static final String CONFIG_LOCATION = "beans.xml";
 
 
     public static void main(String[] args) {
@@ -16,7 +17,7 @@ public class Main {
         log.info("Inside the guess number game ");
 
         //create context (container )
-        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
         NumberGenerator numberGenerator = ctx.getBean(NumberGenerator.class);
 
         //call method next() to get the next number
