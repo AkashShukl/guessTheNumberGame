2a54ca7a2f4f3692d72480b8fa32eec666b52371
shuklaa884 <shuklaa884@gmail.com>
2021-03-20 12:56:49 +0530

    removed annotations from game and number generator implementation files

--- core/src/main/java/learn/application/GameImpl.java
+++ core/src/main/java/learn/application/GameImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-@Component
+
 public class GameImpl implements Game {
 
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
