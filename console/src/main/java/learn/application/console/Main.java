2b25f276c613bc781c561133aab35c8af35b7148
shuklaa884 <shuklaa884@gmail.com>
2021-03-21 21:42:28 +0530

    removed unnescssary comments form main class

--- console/src/main/java/learn/application/console/Main.java
+++ console/src/main/java/learn/application/console/Main.java
@@ -8,9 +8,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
 
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
-//    private static final String CONFIG_LOCATION = "beans.xml";
-
+    private static final Logger log = LoggerFactory.getLogger(Main.class);//    private static final String CONFIG_LOCATION = "beans.xml";
 
     public static void main(String[] args) {
 
@@ -19,14 +17,6 @@ public class Main {
         //create context (container )
         ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(GameConfig.class);
 
-        //call method next() to get the next number
-//        int num = numberGenerator.next();
-//        //logging the number
-//        log.info("number ={}",num);
-//        //close context (container)
-//        Game game = ctx.getBean(Game.class);
-//        MessageGenerator messageGenerator = ctx.getBean(MessageGenerator.class);
-//        System.out.println(messageGenerator.getMainMessage());
         ctx.close();
 
     }
